97562e67d7602b75ba32ecf789d2335571d36d95
JessQi <85958041+jessq1@users.noreply.github.com>
2021-12-01 14:00:01 -0500

    set up auth middleware

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,10 +2,9 @@ import logger from "pino";
 import dayjs from "dayjs";
 
 const log = logger({
-  prettyPrint: true,
-  base: {
-    pid: false,
-  },
+    transport: {
+        target: 'pino-pretty'
+      },
   timestamp: () => `,"time":"${dayjs().format()}"`,
 });
 
