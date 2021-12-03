00cdc5817b599a65c56dd997b03df47f701e8240
JessQi <85958041+jessq1@users.noreply.github.com>
2021-12-02 23:49:06 -0500

    add api

---
+++ src/config/api.ts
@@ -0,0 +1,20 @@
+import Stripe from "stripe";
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+const stripe = new Stripe(stripeSecretKey, {
+    apiVersion: "2020-08-27",
+    appInfo: { // For sample support and debugging, not required for production:
+      name: "stripe-samples/accept-a-payment",
+      url: "https://github.com/stripe-samples",
+      version: "0.0.2",
+    },
+    typescript: true,
+  });
+
+const paymentIntent = stripe.paymentIntents.create({
+  amount: 1000,
+  currency: 'usd',
+  payment_method_types: ['card'],
+  receipt_email: 'jenny.rosen@example.com',
+});
